3cf6b9b15f3773540701f88130db8aa078d01156
samyuktha <samyuktha0511@gmail.com>
2022-11-07 15:24:52 +0530

    Assignment 3

--- Assignments/Samyuktha/Assignment 2/app.py
+++ Assignments/Samyuktha/Assignment 2/app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template, url_for
+from flask import Flask
+from flask import request, redirect, url_for, render_template
+import ibm_db
 
 app = Flask(__name__)
+
+db_connection = ibm_db.connect("DATABASE=bludb;HOSTNAME=9938aec0-8105-433e-8bf9-0fbb7e483086.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=32459;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=jqk44878;PWD=q4Y1FZWLAnlINU5j",'','')
+#print("success: ", db_connection)
 
 @app.route('/')
 def index():
@@ -10,13 +15,31 @@ def index():
 def about():
     return render_template('About.html')
 
-@app.route("/sign-in")
+@app.route("/sign-in", methods=["GET","POST"])
 def signIn():
-    return render_template('Sign-in.html')
+    if request.method == "POST":
+        data = request.form.to_dict()
+        sql_query = "SELECT userPassword FROM USERS WHERE email = '{}'".format(data["email"])
+        result = ibm_db.exec_immediate(db_connection,sql_query)
+        value = ibm_db.fetch_tuple(result)[0].replace(" ","")
 
-@app.route("/sign-up")
+        if value == data["password"]:
+            return redirect(url_for("index", msg="Logged in"))
+        else:
+            return render_template('Sign-in.html', msg="Invalid credentials")
+
+    if request.method == "GET":
+        return render_template('Sign-in.html')
+
+@app.route("/sign-up", methods=["GET","POST"])
 def signUp():
-    return render_template('Sign-up.html')
+    if request.method == "GET":
+        return render_template('Sign-up.html')
+    if request.method == "POST":
+        data = request.form.to_dict()
+        sql_query = "INSERT INTO USERS (email, username, userPassword) VALUES('{}','{}','{}')".format(data["email"], data["username"], data["password"])
+        ibm_db.exec_immediate(db_connection, sql_query)
+        return redirect(url_for("signIn"))
 
 if __name__ == "__main__":
     app.run(debug=True)
