f60885bf30e65540723bf2885b446e2862d9bc93
sanjanasethuraman <sanjana180501@gmail.com>
2022-11-14 01:11:14 +0530

    CSS changes, navbar and login UI & routing

--- Project Development Phase/Sprint 1/app/views.py
+++ Project Development Phase/Sprint 1/app/views.py
@@ -3,40 +3,48 @@ from flask import render_template
 
 @app.route('/')
 def home():
-    return  render_template('home.html')
+    return render_template('home.html')
+
+@app.route('/sign-in')
+def signIn():
+    return render_template('Signin.html')
+
+@app.route('/sign-up')
+def signUp():
+    return render_template('Signup.html')
 
 @app.route('/headlines')
 def headlines():
-    return  render_template('headlines.html')
+    return render_template('articles.html')
 
 @app.route('/articles')
 def articles():
-    return  render_template('articles.html')
+    return render_template('articles.html')
 
 @app.route('/sources')
 def sources():
-    return  render_template('sources.html')
+    return render_template('articles.html')
 
 @app.route('/category/business')
 def business():
-    return  render_template('business.html')
+    return render_template('articles.html')
 
 @app.route('/category/tech')
 def tech():
-    return  render_template('tech.html')
+    return render_template('articles.html')
 
 @app.route('/category/entertainment')
 def entertainment():
-    return  render_template('entertainment.html')
+    return render_template('articles.html')
 
 @app.route('/category/science')
 def science():
-    return  render_template('science.html')
+    return render_template('articles.html')
 
 @app.route('/category/sports')
 def sports():
-    return  render_template('sport.html')
+    return render_template('articles.html')
 
 @app.route('/category/health')
 def health():
-    return  render_template('health.html')
+    return render_template('articles.html')
