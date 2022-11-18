91781b9faaf9a1bee7600a90aaf1dbdb9b165cc8
samyuktha0511 <samyuktha0511@gmail.com>
2022-11-19 00:30:23 +0530

    kubernetes deployment files

--- Final Deliverables/Source Code/app/config.py
+++ Final Deliverables/Source Code/app/config.py
@@ -6,7 +6,7 @@ class Config:
     NEWS_BASE_EVERYTHING_URL = 'https://newsapi.org/v2/everything?domains={}&apiKey={}'
     NEWS_BASE_HEADLINES_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey={}'
     NEWS_BASE_SOURCE = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
-    API_KEY = "ccee2e6628404b82b42f3c2b43026b5f"
+    API_KEY = "0b827eeefc5440a1bfb1d73dc65d7250"
 
 class DB:
     DB_URI = ibm_db.connect("DATABASE=bludb;HOSTNAME=9938aec0-8105-433e-8bf9-0fbb7e483086.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=32459;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=jqk44878;PWD=q4Y1FZWLAnlINU5j",'','')
